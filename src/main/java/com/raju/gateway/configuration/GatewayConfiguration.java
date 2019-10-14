fad10a0791c4e67bc853f27be67b18b7d2ca501f
Raju <ramaraju.gva@gmail.com>
2019-10-14 21:38:43 +0530

    Initial Gateway configuration

---
+++ src/main/java/com/raju/gateway/configuration/GatewayConfiguration.java
@@ -0,0 +1,21 @@
+package com.raju.gateway.configuration;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfiguration {
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/posts/**")
+                        .filters(f->f.rewritePath("/posts/(?<segment>.*)","/posts/${segment}")
+                        .hystrix(c -> c.setName("RapidAPI:Words")))
+                        .uri("https://jsonplaceholder.typicode.com")
+                        .id("posts-service"))
+                .build();
+    }
+
+}
