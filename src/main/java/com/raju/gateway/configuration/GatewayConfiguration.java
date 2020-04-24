22fef7e0734210d464ba4fbefd1af876fa1522e0
Raju <ramaraju.gva@gmail.com>
2020-04-24 14:25:04 +0530

    Refactor

--- src/main/java/com/raju/gateway/configuration/GatewayConfiguration.java
+++ src/main/java/com/raju/gateway/configuration/GatewayConfiguration.java
@@ -7,15 +7,19 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GatewayConfiguration {
-    @Bean
-    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route(r -> r.path("/posts/**")
-                        .filters(f->f.rewritePath("/posts/(?<segment>.*)","/posts/${segment}")
-                        .hystrix(c -> c.setName("RapidAPI:Words")))
-                        .uri("https://jsonplaceholder.typicode.com")
-                        .id("posts-service"))
-                .build();
-    }
-
+  @Bean
+  public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+    return builder
+        .routes()
+        .route(
+            r ->
+                r.path("/posts/**")
+                    .filters(
+                        f ->
+                            f.rewritePath("/posts/(?<segment>.*)", "/posts/${segment}")
+                                .hystrix(c -> c.setName("RapidAPI:Words")))
+                    .uri("https://jsonplaceholder.typicode.com")
+                    .id("posts-service"))
+        .build();
+  }
 }
